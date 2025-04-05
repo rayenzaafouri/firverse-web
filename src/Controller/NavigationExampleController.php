c0adb22dc7298e71eff4b963515a1eac1f53ee41
Mohamed Rayen Zaafouri <mohamedrayenzaafouri@gmail.com>
2025-04-05 13:19:48 +0100

    feat: added /dbtest route to echo all users

--- src/Controller/NavigationExampleController.php
+++ src/Controller/NavigationExampleController.php
@@ -2,17 +2,16 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class NavigationExampleController extends AbstractController
 {
-    #[Route('/example', name: 'app_navigation_example')]
-    public function index(): Response
+    #[Route('/dbtest')]
+    public function index(UserRepository $userRepo): Response
     {
-        return $this->render('navigation_example/index.html.twig', [
-            'controller_name' => 'NavigationExampleController',
-        ]);
+        dd($userRepo->findAll());
     }
 }
