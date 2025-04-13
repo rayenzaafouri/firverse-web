892298b83f885746a9a5cbe70c7bb7be90c1d23c
medalitlili <mouhamedalitlili@esprit.tn>
2025-04-13 19:50:57 +0100

    crud back office table gym terminé

---
+++ src/Form/MembershipType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Gym;
+use App\Entity\Membership;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+
+class MembershipType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('start_date', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('end_date', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('price', NumberType::class, [
+                'required' => true,  // ou false selon ton besoin
+                'attr' => ['step' => 'any'], // permet de saisir des décimales
+            ])
+            ->add('coaching')
+            ->add('status')
+            ->add('gym', EntityType::class, [
+                'class' => Gym::class,
+                'choice_label' => 'id',
+            ])
+            ->add('user', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Membership::class,
+        ]);
+    }
+}
