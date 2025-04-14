215120004786df287a5b1402d842ac54c7fc3839
medalitlili <mouhamedalitlili@esprit.tn>
2025-04-14 17:23:18 +0100

    front office user init

---
+++ src/Controller/GymFrontController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\GymRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GymFrontController extends AbstractController
+{
+    #[Route('/gyms', name: 'gym_front_list')]
+    public function list(GymRepository $gymRepository): Response
+    {
+        $gyms = $gymRepository->findAll();
+        return $this->render('gym_front/front.html.twig', [
+            'gyms' => $gyms,
+        ]);
+    }
+    #[Route('/gyms/{id}', name: 'gym_front_detail')]
+    public function detail(int $id, GymRepository $gymRepository): Response
+    {
+        $gym = $gymRepository->find($id);
+
+        if (!$gym) {
+            throw $this->createNotFoundException('Salle non trouvée.');
+        }
+
+        return $this->render('gym_front/detail.html.twig', [
+            'gym' => $gym,
+        ]);
+    }
+
+}
